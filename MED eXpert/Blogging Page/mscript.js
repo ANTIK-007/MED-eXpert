622be3ce7bbb5752cfa98c47440d0dc2291c10ef
ANTIK MONDAL <antikmondal123@gmail.com>
2025-04-18 00:18:42 +0530

    All files are uploaded v.01

---
+++ MED eXpert/Blogging Page/mscript.js
@@ -0,0 +1,19 @@
+// Remove this Swiper initialization code
+var swiper = new Swiper(".mySwiperservices", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
